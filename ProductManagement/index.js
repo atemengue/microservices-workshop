20507cd8ee19210b49e940cd807b51db87d0b7bc
atemengue <moafembe@gmail.com>
2024-07-21 05:26:50 +0100

    setup connection

--- ProductManagement/index.js
+++ ProductManagement/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const { mongoConnect } = require('./config/sync');
 
 // init express app
 const app = express();
 
 app.use(bodyParser.json());
 
+
+// database connection
+mongoConnect();
 
 // configure swagger
 require('./swagger')(app)
