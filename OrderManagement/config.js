c3a09e0b5badb54379fbf4ede4af88d3a81a66c7
atemengue <moafembe@gmail.com>
2024-08-13 04:24:02 +0100

    async com start todo

--- OrderManagement/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config()
-
-const config = {
-  INVENTORY_SERVICE: process.env.INVENTORY_SERVICE_URL,
-  PAYEMENT_SERVICE: process.env.PAYEMENT_SERVICE_URL,
-}
-
-module.exports = config;
