f969559ea732037728f4af54525e01cc32f85679
atemengue <moafembe@gmail.com>
2024-08-14 09:25:03 +0100

    update order management

--- OrderManagement/publisher/publisher.js
+++ OrderManagement/publisher/publisher.js
@@ -1,28 +1,25 @@
 const express = require('express');
-const { connect } = require('../../config/rabbit-connection');
+const { connect } = require('../config/rabbit-connection');
 
 const router = express.Router();
 const queue1 = "order";
 const queue2 = "product"
 
 
-async function sendUpdateInventoryMessage(){
-    const channel = await connect();
-    const message = JSON.stringify({
-        action: "inventoryUpdated",
-        productId: productId,
-        payload: {
-          quantity: -quantity,
-        }
-      });
+async function sendUpdateInventoryMessage(data) {
+  const channel = await connect();
+  const message = JSON.stringify({
+    action: "inventoryUpdated",
+    payload: data
+  });
 
-      channel.sendToQueue(queue1, Buffer.from(message));
-      console.log("Sent 'inventoryUpdated' message:", message);
+  channel.sendToQueue(queue1, Buffer.from(message));
+  console.log("Sent 'inventoryUpdated' message:", message);
 }
 
 router.post('/api/order/event', async (req, res) => {
   try {
-    sendUpdateInventoryMessage();
+    sendUpdateInventoryMessage(req.body);
   } catch (error) {
     console.log(error)
     res.status(500).error
