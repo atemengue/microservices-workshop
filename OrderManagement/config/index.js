08f088bc946b7b6b37126b1b667bde887fd1ba89
atemengue <moafembe@gmail.com>
2024-08-15 13:35:07 +0100

    add async ok

---
+++ OrderManagement/config/index.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config()
+
+const config = {
+  INVENTORY_SERVICE: process.env.INVENTORY_SERVICE_URL,
+  PAYEMENT_SERVICE: process.env.PAYEMENT_SERVICE_URL,
+}
+
+module.exports = config;
+
+export default App 
